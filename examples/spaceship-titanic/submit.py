779c6ae85d2c06306b86dca7b309f4e3e14a61bb
Dixing Xu <i@dex.moe>
2025-04-28 21:02:26 +0800

    feat: Update model improvement example
    * directly optimize evaluate.py
    * update README
    * add description.md for reference

---
+++ examples/spaceship-titanic/submit.py
@@ -0,0 +1,12 @@
+import argparse
+import kaggle
+from pathlib import Path
+
+def submit_submission(submission_path: Path):
+    kaggle.api.competition_submit(submission_path, "My first submission with weco", "spaceship-titanic")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--submission-path", "-p", type=Path, default="submission.csv")
+    args = parser.parse_args()
+    submit_submission(args.submission_path)
